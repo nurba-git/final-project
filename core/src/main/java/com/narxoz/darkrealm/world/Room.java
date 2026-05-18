afd12675c014d9c9273e3cfa884abbcb62a64a74
Nurali Seifulla <nurali.seifulla@narxoz.kz>
2026-05-18 15:18:48 +0500

    update  Room

--- core/src/main/java/com/narxoz/darkrealm/world/Room.java
+++ core/src/main/java/com/narxoz/darkrealm/world/Room.java
@@ -1,4 +1,14 @@
 package com.narxoz.darkrealm.world;
 
+import com.narxoz.darkrealm.C;
+import java.util.*;
+
 public class Room {
+    public int x, y, w, h;
+    public Room(int x,int y,int w,int h){ this.x=x; this.y=y; this.w=w; this.h=h; }
+    public int centerX(){ return x+w/2; }
+    public int centerY(){ return y+h/2; }
+    public boolean overlaps(Room o){
+        return x<o.x+o.w+1&&x+w+1>o.x&&y<o.y+o.h+1&&y+h+1>o.y;
+    }
 }
